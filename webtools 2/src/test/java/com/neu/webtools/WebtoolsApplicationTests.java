76a660efd32527ced3b476825070b397db2ac52c
HarshalJaiswalNEU <jaiswal.ha@northeastern.edu>
2022-05-01 21:50:34 -0400

    updated project

---
+++ webtools 2/src/test/java/com/neu/webtools/WebtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.webtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
